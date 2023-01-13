b25a8ff55972676770722d8d372f630f2765c263
Edson Sousa <edson3711.es@gmail.com>
2023-01-13 09:39:56 -0300

    aula 5

---
+++ 2_projeto_vue/src/App.vue
@@ -0,0 +1,21 @@
+ <template>
+  <div>
+    <PrimeiroComponente />
+    <p>O dado de app é : {{ testando }}</p>
+  </div>
+</template>
+
+ <script>
+import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+export default {
+  name: "App",
+  components: {
+    PrimeiroComponente,
+  },
+  data() {
+    return {
+      testando: "teste",
+    };
+  },
+};
+</script>
---
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Ola vue</h1>
+    <p>Meu nome é {{ nome }} e trabalho com {{ profissao }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "PrimeiroComponente",
+  data() {
+    return {
+      nome: "Edson",
+      profissao:"programador"
+    };
+  },
+};
+</script>
