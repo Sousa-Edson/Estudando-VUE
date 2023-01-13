5cfcf15c415bebd37e618c4d9674e58ad8faf86b
Edson Sousa <edson3711.es@gmail.com>
2023-01-13 19:13:55 -0300

    aula 7

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -2,20 +2,23 @@
   <div>
     <PrimeiroComponente />
     <p>O dado de app é : {{ testando }}</p>
+    <LifeCycle/>
   </div>
 </template>
 
  <script>
 import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+import LifeCycle from './components/LifeCycle.vue'
 export default {
   name: "App",
   components: {
     PrimeiroComponente,
+    LifeCycle
   },
   data() {
     return {
       testando: "teste",
     };
-  },
+  }
 };
 </script>
